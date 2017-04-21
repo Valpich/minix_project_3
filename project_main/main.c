bc45d9aae8c81ff3b1d8f1702244c6aabb87dbd4
Valentin Pichavant <valentin.pichavant@hotmail.fr>
2017-04-21 18:18:46 -0500

    sfsd

--- project_main/main.c
+++ project_main/main.c
@@ -2,47 +2,33 @@
 #include <stdlib.h>
 #include "repair.h"
 
-#include "fs.h"
-#include <sys/types.h>
-#include <ctype.h>
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <minix/ipc.h>
-#include "const.h"
-#include "inode.h"
-#include "type.h"
-#include "mfsdir.h"
-#include "super.h"
-#include <unistd.h>
-#include <time.h>
-#include <limits.h>
-#include <errno.h>
-#include "buf.h"
-
-#include <sys/stat.h>
-#include <dirent.h>
 
 /*===========================================================================*
  *				main                                         *
  *===========================================================================*/
 int main(int argc, char *argv[]){
-
-    printf("Getting super node from device %llu ...\n", fs_dev);
-
-    sb = get_super(fs_dev);int x=inodewalker(fs_dev);
+    int * r=calloc(1017088,4);
     
-    //printf("x: %d\n",x);
-    //printf("r: %d  %d  %d\n",r[0],r[1],r[2]);
+    int x=inodewalker(r);
+    
+    printf("x: %d\n",x);
+    printf("r: %d  %d  %d\n",r[0],r[1],r[2]);
     
     FILE * file=fopen("blocks.txt","w");
     
     int i=0;
-
+    while(r[i]!=0){
+        i++;
+    }
+    printf("number of blocks referenced by inodes: %d",i);
+    i=0;
+    while(r[i]!=0){
+        fprintf(file,"%d",r[i]);
+        i++;
+    }
     
     fclose(file);
-   // free(r);
+    free(r);
     printf("Blocks referenced by inodes saved in blocks.txt\n");
     return 0;
 }
