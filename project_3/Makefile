9483121b45ac62864f7cf5ec584f3e35a59b6f15
Valentin Pichavant <valentin.pichavant@hotmail.fr>
2017-04-21 14:13:05 -0500

    sefssdf

---
+++ project_3/Makefile
@@ -0,0 +1,2 @@
+cc main.c -o main
+./main
---
+++ project_3/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <ctype.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <time.h>
+#include <limits.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <minix/ipc.h>
+#include "fs.h"
+#include "const.h"
+#include "inode.h"
+#include "mfsdir.h"
+#include "super.h"
+#include "type.h"
+#include "buf.h"
+
+
+int main() {
+    printf("Hello, World");
+    getbitmaps();
+    lsuper();
+    printf("Hello, World2");
+    return 0;
+}
