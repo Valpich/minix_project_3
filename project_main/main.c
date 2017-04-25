8496364ad968a94c616497a1cff06d4d975933b6
Valentin Pichavant <valentin.pichavant@hotmail.fr>
2017-04-24 22:55:07 -0500

    fsdf

--- project_main/main.c
+++ project_main/main.c
@@ -11,27 +11,9 @@ int main(int argc, char *argv[]){
     
     int x=inodewalker(r);
     
-    printf("x: %d\n",x);
-
-    printf("r: %d  %d  %d\n",r[0],r[1],r[2]);
-    
-    FILE * file=fopen("blocks.txt","w");
-    
-    int i=0;
-    while(r[i]!=0){
-        i++;
-    }
-    printf("number of blocks referenced by inodes: %d\n",i);
-    i=0;
-    while(r[i]!=0){
-        fprintf(file,"%d\n",r[i]);
-        i++;
-    }
-    
-    fclose(file);
-    free(r);
-    printf("Blocks referenced by inodes saved in blocks.txt\n");
     int z=zonewalker(r);
     directorywalker("hello");
+    recovery(r);
+    damage(r,2)
     return 0;
 }
