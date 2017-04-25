c357b57bcdfcf749b7bb37a0fbcfd21682128674
Valentin Pichavant <valentin.pichavant@hotmail.fr>
2017-04-25 16:05:08 -0500

    dasd

--- servers/mfs/fs_repair.c
+++ servers/mfs/fs_repair.c
@@ -829,6 +829,7 @@ int fs_damage(void){
         compare_bitmaps(zmap_disk, imap_disk, N_IMAP, list);
         damage_bitmap(imap_disk, N_IMAP, IMAP, inode);
         compare_bitmaps(zmap_disk, imap_disk, N_IMAP, list);
+        dumpbitmap(imap_disk, BLK_IMAP, N_IMAP);
     }
     puts("fs_damage ended with success");
     return 1;
