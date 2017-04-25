a4155c96dd33494291af6739763f26a7ab3ba32f
AFA16SCM17R <aregnier@hawk.iit.edu>
2017-04-25 15:09:04 -0500

    print

--- project_main/ANTOINE/debug.c
+++ project_main/ANTOINE/debug.c
@@ -14,6 +14,38 @@
 #include <sys/stat.h>
 #include <sys/stat.h>
 
+/* Print the pathname given by a linked list pointed to by `sp'.  The
+ * names are in reverse order.
+ */
+void printrec(struct stack *sp)
+{
+  if (sp->st_next != 0) {
+	printrec(sp->st_next);
+	putchar('/');
+	printname(sp->st_dir->mfs_d_name);
+  }
+}
+
+/* Print the current pathname.  */
+void printpath(mode, nlcr)
+int mode;
+int nlcr;
+{
+  if (ftop->st_next == 0)
+	putchar('/');
+  else
+	printrec(ftop);
+  switch (mode) {
+      case 1:
+	printf(" (ino = %u, ", ftop->st_dir->d_inum);
+	break;
+      case 2:
+	printf(" (ino = %u)", ftop->st_dir->d_inum);
+	break;
+  }
+  if (nlcr) printf("\n");
+}
+
 int main ()
 {
     printpath(1, 0);
