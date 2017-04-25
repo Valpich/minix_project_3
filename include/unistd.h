17cedf16a94723d4f5c40036387c1f7eead99558
Valentin Pichavant <valentin.pichavant@hotmail.fr>
2017-04-25 14:47:05 -0500

    sz

--- include/unistd.h
+++ include/unistd.h
@@ -321,6 +321,7 @@ int	unlinkat(int, const char *, int);
 /*
  * Implementation-defined extensions
  */
+#ifdef __minix
 int lseek64(int fd, u64_t _offset, int _whence, u64_t *_newpos);
 #if defined(_MINIX)
 #include <minix/type.h>
@@ -337,6 +338,7 @@ int getnucred(endpoint_t proc_ep, struct ucred *ucred);
 ssize_t pread64(int fd, void *buf, size_t count, u64_t where);
 ssize_t pwrite64(int fd, const void *buf, size_t count, u64_t where);
 #endif /* __MINIX */
+#endif /* __minix */
 
 #if defined(_NETBSD_SOURCE)
 #ifndef __minix
