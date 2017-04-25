db9127d5b1f49f01f5a1691492a8ab7bd41835ea
Valentin Pichavant <valentin.pichavant@hotmail.fr>
2017-04-25 18:12:08 -0500

    salsa

--- servers/mfs/fs_repair.c
+++ servers/mfs/fs_repair.c
@@ -839,7 +839,7 @@ int fs_damage(void){
         damage_bitmap(imap_disk, N_IMAP, IMAP, inode);
         compare_bitmaps(zmap_disk, imap_disk, N_IMAP, list);
         printf("BLK_IMAP is %d N_IMAP is %d.\n",BLK_IMAP, N_IMAP);
-        write_super(sb)
+        dumpbitmap(imap_disk, BLK_IMAP, N_IMAP);
     }
     puts("fs_damage ended with success");
     return 1;
