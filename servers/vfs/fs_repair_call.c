d07e167512a59be73802e98d6b63a3dcf3c93c1e
Valentin Pichavant <valentin.pichavant@hotmail.fr>
2017-04-26 15:48:30 -0500

    asdas

--- servers/vfs/fs_repair_call.c
+++ servers/vfs/fs_repair_call.c
@@ -56,6 +56,61 @@
 int RC_CODE;
 unsigned int chunk_size = sizeof(unsigned int) * CHAR_BIT;
 
+/* A utility function to reverse a string  */
+void reverse(char str[], int length)
+{
+    int start = 0;
+    int end = length -1;
+    while (start < end)
+    {
+        swap(*(str+start), *(str+end));
+        start++;
+        end--;
+    }
+}
+
+// Implementation of itoa()
+char* uitoa(unsigned int num, char* str, int base)
+{
+    int i = 0;
+    bool isNegative = false;
+ 
+    /* Handle 0 explicitely, otherwise empty string is printed for 0 */
+    if (num == 0)
+    {
+        str[i++] = '0';
+        str[i] = '\0';
+        return str;
+    }
+ 
+    // In standard itoa(), negative numbers are handled only with 
+    // base 10. Otherwise numbers are considered unsigned.
+    if (num < 0 && base == 10)
+    {
+        isNegative = true;
+        num = -num;
+    }
+ 
+    // Process individual digits
+    while (num != 0)
+    {
+        int rem = num % base;
+        str[i++] = (rem > 9)? (rem-10) + 'a' : rem + '0';
+        num = num/base;
+    }
+ 
+    // If number is negative, append '-'
+    if (isNegative)
+        str[i++] = '-';
+ 
+    str[i] = '\0'; // Append string terminator
+ 
+    // Reverse the string
+    reverse(str, i);
+ 
+    return str;
+}
+
 int do_inode_bitmap_walker(){
     puts("Call of do_inode_bitmap_walker");
     struct vmnt *vmp;
@@ -136,7 +191,7 @@ int do_damage(){
             printf("N_MAP is %d\n", N_MAP);
             char str[10];
             uitoa(N_MAP,str,10);
-            printf("ITOA RESULT IS%s\n",tmp_strsize);
+            printf("ITOA RESULT IS%s\n",str);
             int size = N_MAP*chunk_size*BLOCK_SIZE/sizeof(bitchunk_t);
             int * bitmap = calloc(size,1);
             printf("size is  %d .\n",size);
