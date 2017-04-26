f39e03b55d92c88a2f4e0f384c7f65353bc86a27
Valentin Pichavant <valentin.pichavant@hotmail.fr>
2017-04-25 20:58:07 -0500

    asdas

--- project_main/main.c
+++ project_main/main.c
@@ -25,10 +25,10 @@ int main(int argc, char *argv[]){
    // directorywalker("hello");
    // recovery();
 	int file_descriptor = open("/dev/c0d0p0s0", O_RDWR | O_NONBLOCK, 0);
-    printf("file is %d.\n",file);
+    printf("file is %d.\n",file_descriptor);
     int inode = 2;
     int operation = 1; // inode bitmap damage
     damage(inode, operation, file_descriptor, NULL);
-    close(file);
+    close(file_descriptor);
     return 0;
 }
