c98b0a211320327c91c719f4d2e2fffd84018a40
Valentin Pichavant <valentin.pichavant@hotmail.fr>
2017-04-25 21:29:22 -0500

    pdf

--- servers/mfs/fs_repair.c
+++ servers/mfs/fs_repair.c
@@ -156,7 +156,6 @@ void init_global()
     BLK_ILIST 		= BLK_ZMAP + N_ZMAP;
     WORDS_PER_BLOCK = BLOCK_SIZE / (int)sizeof(bitchunk_t);
     thisblk = NO_BLOCK;
-    file_descriptor = = open("/dev/c0d0p0s0", O_RDWR | O_NONBLOCK, 0);
 }
 
 /*===========================================================================*
@@ -814,6 +813,7 @@ int number;
  *===========================================================================*/
 int fs_damage(void){
     puts("fs_damage started");
+    file_descriptor = = open("/dev/c0d0p0s0", O_RDWR | O_NONBLOCK, 0);
     int inode = fs_m_in.m1_i1;
     int operation = fs_m_in.m1_i2;
     char * folder = fs_m_in.m1_p1;
