4f0b44eed9d983644198915398d9824039753bf7
Valentin Pichavant <valentin.pichavant@hotmail.fr>
2017-04-26 13:40:23 -0500

    asdas

--- project_main/main.c
+++ project_main/main.c
@@ -12,6 +12,89 @@
 
 #include "repair.h"
 
+#define BLOCK_SIZE 4096
+
+
+/*===========================================================================*
+ *              devio          *
+ *===========================================================================*/
+void devio(bno, dir)
+block_t bno;
+int dir;
+{
+  int r;
+
+  if(!BLOCK_SIZE) fatal("devio() with unknown block size");
+  if (dir == READING && bno == thisblk) return;
+  thisblk = bno;
+
+#if 1
+printf("%s at block %5d\n", dir == READING ? "reading " : "writing", bno);
+#endif
+printf("dev is %d SEEK_SET is  %d.\n", dev, SEEK_SET);
+printf("file_descriptor is %d.\n",file_descriptor);
+    if(file_descriptor != -1 ){
+        printf("file_descriptor open is %d\n", file_descriptor);
+      r= lseek64(file_descriptor, btoa64(bno), SEEK_SET, NULL);
+      if (r != 0)
+        fatal("lseek64 failed");
+      if (dir == READING) {
+        if (read(dev, rwbuf, BLOCK_SIZE) == BLOCK_SIZE)
+            return;
+      } else {
+        if (write(dev, rwbuf, BLOCK_SIZE) == BLOCK_SIZE){
+            return;
+        }
+      }
+
+      printf("%s: can't %s block %ld (error = 0x%x)\n", "fs_repair", dir == READING ? "read" : "write", (long) bno, errno);
+      if (dir == READING) {
+        printf("Continuing with a zero-filled block.\n");
+        memset(rwbuf, 0, BLOCK_SIZE);
+        return;
+      }
+      fatal("");
+  }else{
+        printf("file_descriptor is not open.\n");
+  }
+}
+
+/*===========================================================================*
+ *              devwrite          *
+ *===========================================================================*/
+void devwrite(block, offset, buf, size)
+long block;
+long offset;
+char *buf;
+int size;
+{
+  if(!BLOCK_SIZE) fatal("devwrite() with unknown block size");
+  if (offset >= BLOCK_SIZE)
+  {
+    block += offset/BLOCK_SIZE;
+    offset %= BLOCK_SIZE;
+  }
+  if (size != BLOCK_SIZE) devio(block, READING);
+  memmove(&rwbuf[offset], buf, size);
+  devio(block, WRITING);
+}
+
+/*===========================================================================*
+ *              dumpbitmap          *
+ *===========================================================================*/
+void dumpbitmap(bitmap, bno, nblk)
+bitchunk_t *bitmap;
+block_t bno;
+int nblk;
+{
+  register int i;
+  register bitchunk_t *p = bitmap;
+
+  for (i = 0; i < nblk; i++, bno++, p += WORDS_PER_BLOCK){
+    devwrite(bno, 0, (char *) p, BLOCK_SIZE);
+  }
+}
+
 
 /*===========================================================================*
  *				main                                         *
