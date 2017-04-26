6810ff8864303f5d606e6396eba3e2671eaaeff2
Valentin Pichavant <valentin.pichavant@hotmail.fr>
2017-04-26 13:08:42 -0500

    sds

--- servers/vfs/fs_repair_call.c
+++ servers/vfs/fs_repair_call.c
@@ -117,7 +117,8 @@ int do_damage(){
     int inode = m_in.m1_i1;
     int operation = m_in.m1_i2;
     char * folder = m_in.m1_p1;
-    endpoint_t endpoint = m_in.m_source;
+  //  char * output = (char *) m_in.m1_p2;
+  //  endpoint_t endpoint = m_in.m_source;
     printf("inode received is %d.\n",inode);
     for (vmp = &vmnt[0]; vmp < &vmnt[NR_MNTS]; ++vmp) {
         if ( strcmp("/home", vmp->m_mount_path) == 0 ) {
