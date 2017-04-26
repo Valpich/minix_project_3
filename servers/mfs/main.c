42a8f8402f9cc1f5d70ebd50ee92b94dacfd1d4b
Valentin Pichavant <valentin.pichavant@hotmail.fr>
2017-04-25 22:12:23 -0500

    fs

--- servers/mfs/main.c
+++ servers/mfs/main.c
@@ -33,9 +33,7 @@ int main(int argc, char *argv[])
  * sending the reply. The loop never terminates, unless a panic occurs.
  */
   int error = OK, ind, transid;
-  file_descriptor = open("/dev/c0d0p0s0", O_RDWR | O_NONBLOCK, 0);
-  printf("file_descriptor is %d\n.",file_descriptor);
-  sleep(20);
+  
   /* SEF local startup. */
   env_setargs(argc, argv);
   sef_local_startup();
