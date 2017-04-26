e1f78dad5dfa1f8ee6f9ec2f8fed0935755ee0d8
Valentin Pichavant <valentin.pichavant@hotmail.fr>
2017-04-26 15:53:36 -0500

    dasd

--- servers/vfs/fs_repair_call.c
+++ servers/vfs/fs_repair_call.c
@@ -70,7 +70,7 @@ unsigned int chunk_size = sizeof(unsigned int) * CHAR_BIT;
  }
 
 /* itoa:  convert n to characters in s */
- void itoa(int n, char s[])
+ void my_itoa(int n, char s[])
  {
     int i, sign;
  
@@ -165,7 +165,7 @@ int do_damage(){
             int N_MAP = m.RES_NBYTES;
             printf("N_MAP is %d\n", N_MAP);
             char str[10];
-            itoa(N_MAP,str);
+            my_itoa(N_MAP,str);
             printf("ITOA RESULT IS%s\n",str);
             int size = N_MAP*chunk_size*BLOCK_SIZE/sizeof(bitchunk_t);
             int * bitmap = calloc(size,1);
