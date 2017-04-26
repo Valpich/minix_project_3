b00477321fe79861147a308db2d46e3db42bb51a
Valentin Pichavant <valentin.pichavant@hotmail.fr>
2017-04-26 16:41:10 -0500

    asdas

--- project_main/main.c
+++ project_main/main.c
@@ -242,10 +242,10 @@ int inode;
  *===========================================================================*/
 int main(int argc, char *argv[]){
     
-   // int x=inodewalker();
+    int x=inodewalker();
    // int z=zonewalker();
    // directorywalker("hello");
    // recovery();
-	damage_inode(2);
+	//damage_inode(2);
     return 0;
 }
