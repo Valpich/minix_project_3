61b7eb82b336559667da07c1099cb22261c3e16f
AFA16SCM17R <aregnier@hawk.iit.edu>
2017-04-26 21:08:05 -0500

    t4.5

--- project_main/ANTOINE/debug.c
+++ project_main/ANTOINE/debug.c
@@ -1,65 +1,141 @@
-#include <sys/types.h>
-#include <ctype.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <limits.h>
+/* We want POSIX.1-2008 + XSI, i.e. SuSv4, features */
+#define _XOPEN_SOURCE 700
+
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
-
+#include <ftw.h>
+#include <time.h>
 #include <stdio.h>
-#include <sys/stat.h>
-#include <a.out.h>
-#include <dirent.h>
+#include <string.h>
+#include <errno.h>
 
-/* Print all the dirs starting from <path> [maybe recursive]. */
-int print_dirs(const char *path, int recursive)
+
+/* POSIX.1 says each process has at least 20 file descriptors.
+ * Three of those belong to the standard streams.
+ * Here, we use a conservative estimate of 15 available;
+ * assuming we use at most two for other uses in this program,
+ * we should never run into any problems.
+ * Most trees are shallower than that, so it is efficient.
+ * Deeper trees are traversed fine, just a bit slower.
+ * (Linux allows typically hundreds to thousands of open files,
+ *  so you'll probably never see any issues even if you used
+ *  a much higher value, say a couple of hundred, but
+ *  15 is a safe, reasonable value.)
+*/
+#ifndef USE_FDS
+#define USE_FDS 15
+
+int print_entry(const char *filepath, const struct stat *info,
+                const int typeflag, struct FTW *pathinfo)
 {
-    struct dirent *direntp = NULL;
-    DIR *dirp = NULL;
-    size_t path_len;
+    /* const char *const filename = filepath + pathinfo->base; */
+    const double bytes = (double)info->st_size; /* Not exact if large! */
+    struct tm mtime;
 
-    /* Check input parameters. */
-    if (!path)
-        return -1;
-    path_len = strlen(path);
+    localtime_r(&(info->st_mtime), &mtime);
 
-    if (!path || !path_len || (path_len > _POSIX_PATH_MAX))
-        return -1;
+    printf("%04d-%02d-%02d %02d:%02d:%02d",
+           mtime.tm_year+1900, mtime.tm_mon+1, mtime.tm_mday,
+           mtime.tm_hour, mtime.tm_min, mtime.tm_sec);
 
-    /* Open directory */
-    dirp = opendir(path);
-    if (dirp == NULL)
-        return -1;
+    if (bytes >= 1099511627776.0)
+        printf(" %9.3f TiB", bytes / 1099511627776.0);
+    else
+    if (bytes >= 1073741824.0)
+        printf(" %9.3f GiB", bytes / 1073741824.0);
+    else
+    if (bytes >= 1048576.0)
+        printf(" %9.3f MiB", bytes / 1048576.0);
+    else
+    if (bytes >= 1024.0)
+        printf(" %9.3f KiB", bytes / 1024.0);
+    else
+        printf(" %9.0f B  ", bytes);
 
-    while ((direntp = readdir(dirp)) != NULL)
-    {
-        /* For every directory entry... */
-        struct stat fstat;
-        char full_name[_POSIX_PATH_MAX + 1];
+        if (typeflag == FTW_SL) {
+        char   *target;
+        size_t  maxlen = 1023;
+        ssize_t len;
 
-        /* Calculate full name, check we are in file length limts */
-        if ((path_len + strlen(direntp->d_name) + 1) > _POSIX_PATH_MAX)
-            continue;
+        while (1) {
 
-        strcpy(full_name, path);
-        if (full_name[path_len - 1] != '/')
-            strcat(full_name, "/");
-        strcat(full_name, direntp->d_name);
+            target = malloc(maxlen + 1);
+            if (target == NULL)
+                return ENOMEM;
 
-            printf("%s\n", full_name);
-            if (recursive)
-                print_dirs(full_name, 1);
-    }
+            len = readlink(filepath, target, maxlen);
+            if (len == (ssize_t)-1) {
+                const int saved_errno = errno;
+                free(target);
+                return saved_errno;
+            }
+            if (len >= (ssize_t)maxlen) {
+                free(target);
+                maxlen += 1024;
+                continue;
+            }
 
-    /* Finalize resources. */
-    (void)closedir(dirp);
-    return 0;
+            target[len] = '\0';
+            break;
+        }
+
+        printf(" %s -> %s\n", filepath, target);
+        free(target);
+
+      } else
+  if (typeflag == FTW_SLN)
+      printf(" %s (dangling symlink)\n", filepath);
+  else
+  if (typeflag == FTW_F)
+      printf(" %s\n", filepath);
+  else
+  if (typeflag == FTW_D || typeflag == FTW_DP)
+      printf(" %s/\n", filepath);
+  else
+  if (typeflag == FTW_DNR)
+      printf(" %s/ (unreadable)\n", filepath);
+  else
+      printf(" %s (unknown)\n", filepath);
+
+  return 0;
 }
 
-/* We are taking first argument as initial path name. */
-int main(int argc, const char* argv[])
+int print_directory_tree(const char *const dirpath)
 {
-    print_dirs(argv[1], 1);
-    return 0;
+    int result;
+
+    /* Invalid directory path? */
+    if (dirpath == NULL || *dirpath == '\0')
+        return errno = EINVAL;
+
+    result = nftw(dirpath, print_entry, USE_FDS, FTW_PHYS);
+    if (result >= 0)
+        errno = result;
+
+    return errno;
+}
+
+int main(int argc, char *argv[])
+{
+    int arg;
+
+    if (argc < 2) {
+
+        if (print_directory_tree(".")) {
+            fprintf(stderr, "%s.\n", strerror(errno));
+            return EXIT_FAILURE;
+        }
+
+    } else {
+
+        for (arg = 1; arg < argc; arg++) {
+            if (print_directory_tree(argv[arg])) {
+                fprintf(stderr, "%s.\n", strerror(errno));
+                return EXIT_FAILURE;
+            }
+        }
+
+    }
+
+    return EXIT_SUCCESS;
 }
