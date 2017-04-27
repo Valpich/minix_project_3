72f66449f938224c0422463cf75071a7d340fcca
AFA16SCM17R <aregnier@hawk.iit.edu>
2017-04-26 21:04:21 -0500

    sad

--- project_main/ANTOINE/debug.c
+++ project_main/ANTOINE/debug.c
@@ -1,53 +1,71 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <sys/types.h>
-#include <string.h>
+#include <ctype.h>
 #include <errno.h>
-#include <unistd.h> // for open
-/* "readdir" etc. are defined here. */
-#include <dirent.h>
-/* limits.h defines "PATH_MAX". */
-#include <limits.h>
-
 #include <fcntl.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
+#include <stdio.h>
 #include <sys/stat.h>
+#include <a.out.h>
+#include <dirent.h>
 
+struct stack {
+  dir_struct *st_dir;
+  struct stack *st_next;
+  char st_presence;
+} *ftop;
 
-/* List the files in "dir_name". */
-static void list_dir (const char * dir_name) {
+/* Print all the dirs starting from <path> [maybe recursive]. */
+int print_dirs(const char *path, int recursive)
+{
+    struct dirent *direntp = NULL;
+    DIR *dirp = NULL;
+    size_t path_len;
 
-  DIR *mydir;
-  struct dirent *myfile;
-  struct stat mystat;
+    /* Check input parameters. */
+    if (!path)
+        return -1;
+    path_len = strlen(path);
 
-  char buf[512];
-  mydir = opendir(dir_name);
-  while((myfile = readdir(mydir)) != NULL) {
-      sprintf(buf, "%s/%s", dir_name, myfile->d_name);
-      stat(buf, &mystat);
-      //printf("%zu",mystat.st_size);
-      printf("DIR: %s\n\t", myfile->d_name);
-      printf("Inode: %d\n\t", mystat.st_ino);
-      if ((mystat.st_mode & S_IFMT) != S_IFDIR) {
-        printf("NO DIR\n");
-      }else{
-        printf("DIR\n");
-        list_dir(myfile->d_name);
-      }
-  }
+    if (!path || !path_len || (path_len > _POSIX_PATH_MAX))
+        return -1;
 
-  closedir(mydir);
+    /* Open directory */
+    dirp = opendir(path);
+    if (dirp == NULL)
+        return -1;
+
+    while ((direntp = readdir(dirp)) != NULL)
+    {
+        /* For every directory entry... */
+        struct stat fstat;
+        char full_name[_POSIX_PATH_MAX + 1];
+
+        /* Calculate full name, check we are in file length limts */
+        if ((path_len + strlen(direntp->d_name) + 1) > _POSIX_PATH_MAX)
+            continue;
+
+        strcpy(full_name, path);
+        if (full_name[path_len - 1] != '/')
+            strcat(full_name, "/");
+        strcat(full_name, direntp->d_name);
+
+            printf("%s\n", full_name);
+            if (recursive)
+                print_dirs(full_name, 1);
+    }
+
+    /* Finalize resources. */
+    (void)closedir(dirp);
+    return 0;
 }
 
-
-
-int main ()
+/* We are taking first argument as initial path name. */
+int main(int argc, const char* argv[])
 {
-    char * p = malloc(sizeof(char) * 128);
-    printf("Enter you directory: ");
-    scanf("%126s",p);
-
-    list_dir (p);
+    print_dirs(argv[1], 1);
     return 0;
 }
