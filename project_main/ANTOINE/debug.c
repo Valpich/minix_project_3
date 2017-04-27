a30215f0603a3abbc46c6e114ab3d005f4ca417f
AFA16SCM17R <aregnier@hawk.iit.edu>
2017-04-26 19:09:31 -0500

    minix la pute

--- project_main/ANTOINE/debug.c
+++ project_main/ANTOINE/debug.c
@@ -14,39 +14,112 @@
 #include <sys/stat.h>
 #include <sys/stat.h>
 
-/* Print the pathname given by a linked list pointed to by `sp'.  The
- * names are in reverse order.
- */
-void printrec(struct stack *sp)
-{
-  if (sp->st_next != 0) {
-	printrec(sp->st_next);
-	putchar('/');
-	printname(sp->st_dir->mfs_d_name);
+int inodeFinder(char* dir, char* file) {
+
+  int len   = strlen(dir) + strlen(file) + 2;
+  char *total = malloc(len);
+
+  strlcat(total, dir, len);
+  strlcat(total, "/", len);
+  strlcat(total, file, len);
+
+  int inode;
+  int fd;
+
+  fd = open(total, O_RDONLY);
+
+
+  if (fd < 0) {
+      // some error occurred while opening the file
+      // use [perror("Error opening the file");] to get error description
   }
+
+  struct stat file_stat;
+  int ret;
+  ret = fstat (fd, &file_stat);
+  if (ret < 0) {
+     // error getting file stat
+  }
+
+  return inode = file_stat.st_ino;
 }
 
-/* Print the current pathname.  */
-void printpath(mode, nlcr)
-int mode;
-int nlcr;
-{
-  if (ftop->st_next == 0)
-	putchar('/');
-  else
-	printrec(ftop);
-  switch (mode) {
-      case 1:
-	printf(" (ino = %u, ", ftop->st_dir->d_inum);
-	break;
-      case 2:
-	printf(" (ino = %u)", ftop->st_dir->d_inum);
-	break;
+/* List the files in "dir_name". */
+static void list_dir (const char * dir_name) {
+    DIR * d;
+
+    /* Open the directory specified by "dir_name". */
+
+    d = opendir (dir_name);
+
+    /* Check it was opened. */
+    if (! d) {
+        fprintf (stderr, "Cannot open directory '%s': %s\n",
+                 dir_name, strerror (errno));
+        exit (EXIT_FAILURE);
+    }
+    while (1) {
+        struct dirent * entry;
+        const char * d_name;
+
+        /* "Readdir" gets subsequent entries from "d". */
+        entry = readdir (d);
+        if (! entry) {
+            /* There are no more entries in this directory, so break
+               out of the while loop. */
+            break;
+        }
+        d_name = entry->d_name;
+        /* Print the name of the file and directory. */
+	printf ("Inode: %d, Dir-Name: %s/%s\n", inodeFinder(dir_name, d_name), dir_name, d_name);
+
+#if 0
+	/* If you don't want to print the directories, use the
+	   following line: */
+
+        if (! (entry->d_type & DT_DIR)) {
+	    printf ("%s/%s\n", dir_name, d_name);
+	}
+
+#endif /* 0 */
+
+struct stat s;
+stat(entry->d_name, &s);
+if ((s.st_mode & S_IFMT) != S_IFDIR) {
+
+      /* Check that the directory is not "d" or d's parent. */
+
+      if (strcmp (d_name, "..") != 0 &&
+          strcmp (d_name, ".") != 0) {
+          int path_length;
+          char path[PATH_MAX];
+
+          path_length = snprintf (path, PATH_MAX,
+                                  "%s/%s", dir_name, d_name);
+          printf ("%s\n", path);
+          if (path_length >= PATH_MAX) {
+              fprintf (stderr, "Path length has got too long.\n");
+              exit (EXIT_FAILURE);
+          }
+          /* Recursively call "list_dir" with the new path. */
+          list_dir (path);
+      }
   }
-  if (nlcr) printf("\n");
+
+    }
+    /* After going through all the entries, close the directory. */
+    if (closedir (d)) {
+        fprintf (stderr, "Could not close '%s': %s\n",
+                 dir_name, strerror (errno));
+        exit (EXIT_FAILURE);
+    }
 }
 
 int main ()
 {
-    printpath(1, 0);
+    char * p = malloc(sizeof(char) * 128);
+    printf("Enter you directory: ");
+    scanf("%126s",p);
+    list_dir (p);
+    return 0;
 }
