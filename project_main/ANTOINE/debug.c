7de25cbc42667fd52a3485015c23422c4c8596e6
AFA16SCM17R <aregnier@hawk.iit.edu>
2017-04-26 21:14:14 -0500

    bite free

--- project_main/ANTOINE/debug.c
+++ project_main/ANTOINE/debug.c
@@ -40,6 +40,8 @@ int print_entry(const char *filepath, const struct stat *info,
            mtime.tm_hour, mtime.tm_min, mtime.tm_sec);*/
     //lol
 
+    /*
+
     if (bytes >= 1099511627776.0)
         printf(" %9.3f TiB", bytes / 1099511627776.0);
     else
@@ -53,6 +55,8 @@ int print_entry(const char *filepath, const struct stat *info,
         printf(" %9.3f KiB", bytes / 1024.0);
     else
         printf(" %9.0f B  ", bytes);
+
+    */
 
         if (typeflag == FTW_SL) {
         char   *target;
