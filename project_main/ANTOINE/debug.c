0280b4d2878e43ad4ecc210000dca4fafd225e82
AFA16SCM17R <aregnier@hawk.iit.edu>
2017-04-26 20:59:46 -0500

    as

--- project_main/ANTOINE/debug.c
+++ project_main/ANTOINE/debug.c
@@ -16,77 +16,28 @@
 
 /* List the files in "dir_name". */
 static void list_dir (const char * dir_name) {
-    DIR * d;
 
+  DIR *mydir;
+  struct dirent *myfile;
+  struct stat mystat;
 
-    char buf[512];
-
-    /* Open the directory specified by "dir_name". */
-
-    d = opendir (dir_name);
-
-    while (1) {
-        struct dirent * entry;
-        const char * d_name;
-        struct stat mystat;
-        char buf[512];
-
-        /* "Readdir" gets subsequent entries from "d". */
-        entry = readdir (d);
-        if (! entry) {
-            /* There are no more entries in this directory, so break
-               out of the while loop. */
-            break;
-        }
-        sprintf(buf, "%s/%s", dir_name, entry->d_name);
-        stat(buf, &mystat);
-        printf("DIR: %s\n\t", entry->d_name);
-        printf("Inode: %d\n", mystat.st_ino);
-
-        d_name = entry->d_name;
-        /* Print the name of the file and directory. */
-	//printf ("Inode: %d, Dir-Name: %s/%s\n", entry->d_ino, dir_name, d_name);
-
-#if 0
-	/* If you don't want to print the directories, use the
-	   following line: */
-
-        if (! (entry->d_type & DT_DIR)) {
-	    printf ("%s/%s\n", dir_name, d_name);
-	}
-
-#endif /* 0 */
-
-struct stat s;
-stat(entry->d_name, &s);
-if ((s.st_mode & S_IFMT) != S_IFDIR) {
-
-      /* Check that the directory is not "d" or d's parent. */
-
-      if (strcmp (d_name, "..") != 0 &&
-          strcmp (d_name, ".") != 0) {
-          int path_length;
-          char path[PATH_MAX];
-
-          path_length = snprintf (path, PATH_MAX,
-                                  "%s/%s", dir_name, d_name);
-          printf ("%s\n", path);
-          if (path_length >= PATH_MAX) {
-              fprintf (stderr, "Path length has got too long.\n");
-              exit (EXIT_FAILURE);
-          }
-          /* Recursively call "list_dir" with the new path. */
-          list_dir (path);
+  char buf[512];
+  mydir = opendir(p);
+  while((myfile = readdir(mydir)) != NULL) {
+      sprintf(buf, "%s/%s", p, myfile->d_name);
+      stat(buf, &mystat);
+      //printf("%zu",mystat.st_size);
+      printf("DIR: %s\n\t", myfile->d_name);
+      printf("Inode: %d\n\t", mystat.st_ino);
+      if ((mystat.st_mode & S_IFMT) != S_IFDIR) {
+        printf("NO DIR\n");
+      }else{
+        printf("DIR\n");
+        list_dir(myfile->d_name);
       }
   }
 
-    }
-    /* After going through all the entries, close the directory. */
-    if (closedir (d)) {
-        fprintf (stderr, "Could not close '%s': %s\n",
-                 dir_name, strerror (errno));
-        exit (EXIT_FAILURE);
-    }
+  closedir(mydir);
 }
 
 
@@ -97,27 +48,6 @@ int main ()
     printf("Enter you directory: ");
     scanf("%126s",p);
 
-    DIR *mydir;
-    struct dirent *myfile;
-    struct stat mystat;
-
-    char buf[512];
-    mydir = opendir(p);
-    while((myfile = readdir(mydir)) != NULL) {
-        sprintf(buf, "%s/%s", p, myfile->d_name);
-        stat(buf, &mystat);
-        //printf("%zu",mystat.st_size);
-        printf("DIR: %s\n\t", myfile->d_name);
-        printf("Inode: %d\n\t", mystat.st_ino);
-        if ((mystat.st_mode & S_IFMT) != S_IFDIR) {
-          printf("DIR\n");
-        }else{
-          printf("NO DIR\n");
-        }
-
-    }
-    closedir(mydir);
-
-    //list_dir (p);
+    list_dir (p);
     return 0;
 }
