a61fd6e24835d959b4ce517c043a2b754eed7de7
AFA16SCM17R <aregnier@hawk.iit.edu>
2017-04-26 22:50:20 -0500

    das

--- project_main/ANTOINE/debug.c
+++ project_main/ANTOINE/debug.c
@@ -1,3 +1,6 @@
+/* We want POSIX.1-2008 + XSI, i.e. SuSv4, features */
+#define _XOPEN_SOURCE 700
+
 #include <stdlib.h>
 #include <unistd.h>
 #include <ftw.h>
@@ -7,38 +10,93 @@
 #include <errno.h>
 #include <dirent.h>
 
+
+
+/* POSIX.1 says each process has at least 20 file descriptors.
+ * Three of those belong to the standard streams.
+ * Here, we use a conservative estimate of 15 available;
+ * assuming we use at most two for other uses in this program,
+ * we should never run into any problems.
+ * Most trees are shallower than that, so it is efficient.
+ * Deeper trees are traversed fine, just a bit slower.
+ * (Linux allows typically hundreds to thousands of open files,
+ *  so you'll probably never see any issues even if you used
+ *  a much higher value, say a couple of hundred, but
+ *  15 is a safe, reasonable value.)
+*/
+#ifndef USE_FDS
 #define USE_FDS 15
+#endif
 
-int print_entry(const char *filepath, const struct stat *info, const int typeflag, struct FTW *pathinfo) {
-    /* const status of file. */
+int print_entry(const char *filepath, const struct stat *info,
+                const int typeflag, struct FTW *pathinfo)
+{
+    /* const char *const filename = filepath + pathinfo->base; */
+    const double bytes = (double)info->st_size; /* Not exact if large! */
 
+        if (typeflag == FTW_SL) {
+        char   *target;
+        size_t  maxlen = 1023;
+        ssize_t len;
 
-    if (typeflag == FTW_SL) {
-      char   *target;
-      printf(" %s -> %s\n", filepath, target);
-      free(target);
-    } else if (typeflag == FTW_SLN) {
+        int status;
+        struct stat st_buf;
+
+        /* Get the status of the file system object. */
+        status = stat (filepath, &st_buf);
+        if (status != 0) {
+            printf ("Error, errno = %d\n", errno);
+            return 1;
+        }
+
+        while (1) {
+
+            target = malloc(maxlen + 1);
+            if (target == NULL)
+                return ENOMEM;
+
+            len = readlink(filepath, target, maxlen);
+            if (len == (ssize_t)-1) {
+                const int saved_errno = errno;
+                free(target);
+                return saved_errno;
+            }
+            if (len >= (ssize_t)maxlen) {
+                free(target);
+                maxlen += 1024;
+                continue;
+            }
+
+            target[len] = '\0';
+            break;
+        }
+
+        printf(" %s -> %s\n", filepath, target);
+        free(target);
+
+      } else
+  if (typeflag == FTW_SLN)
       printf(" %s (dangling symlink)\n", filepath);
-    } else if (typeflag == FTW_F) {
-      //printf("Inode: %llu ", st_buf.st_ino);
-      printf("%s\n", filepath);
-    } else if (typeflag == FTW_D || typeflag == FTW_DP) {
-      //printf("Inode: %llu ", st_buf.st_ino);
-      printf("%s\n", filepath);
-    } else if (typeflag == FTW_DNR){
+  else
+  if (typeflag == FTW_F)
+      printf("Inode: %d %s", inode, filepath);
+  else
+  if (typeflag == FTW_D || typeflag == FTW_DP)
+      printf("Inode: %d %s", inode, filepath);
+  else
+  if (typeflag == FTW_DNR)
       printf(" %s/ (unreadable)\n", filepath);
-    } else {
+  else
       printf(" %s (unknown)\n", filepath);
-    }
 
-    return 0;
+  return 0;
 }
 
 int print_directory_tree(const char *const dirpath)
 {
     int result;
 
-    /* Invalid directory path */
+    /* Invalid directory path? */
     if (dirpath == NULL || *dirpath == '\0')
         return errno = EINVAL;
 
@@ -49,21 +107,26 @@ int print_directory_tree(const char *const dirpath)
     return errno;
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     int arg;
 
     if (argc < 2) {
+
         if (print_directory_tree(".")) {
             fprintf(stderr, "%s.\n", strerror(errno));
             return EXIT_FAILURE;
         }
+
     } else {
+
         for (arg = 1; arg < argc; arg++) {
             if (print_directory_tree(argv[arg])) {
                 fprintf(stderr, "%s.\n", strerror(errno));
                 return EXIT_FAILURE;
             }
         }
+
     }
 
     return EXIT_SUCCESS;
